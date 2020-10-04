f32291ba4435695398cd347fa5648a411f00be6f
anurudhp <anurudhp97@gmail.com>
2020-10-04 14:41:52 +0530

    [ABC159/A] Simplify fold

--- contests/atcoder/abc159/a.hs
+++ contests/atcoder/abc159/a.hs
@@ -1,9 +1,11 @@
 -- AC https://atcoder.jp/contests/abc159/submissions/11095079
 
-import Control.Arrow
+import Control.Arrow ((>>>))
 
-main = interact $ 
-  words >>> map (read:: String->Int) >>> solve >>> show
+main :: IO ()
+main =
+  interact $
+    words >>> map (read :: String -> Int) >>> solve >>> show
 
 solve :: [Int] -> Int
-solve xs = foldl (+) 0 $ map (\x -> div (x * (x - 1)) 2) xs
+solve xs = foldl1 (+) $ (\x -> div (x * (x - 1)) 2) <$> xs
