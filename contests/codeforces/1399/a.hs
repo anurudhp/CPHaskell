cfe0717157d3e328b3c22070e3bad0e184f7a6fb
anurudhp <anurudhp97@gmail.com>
2020-10-21 20:02:45 +0530

    refactor

--- contests/codeforces/1399/a.hs
+++ contests/codeforces/1399/a.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE Safe #-}
 
 import safe Control.Arrow ((>>>))
+import safe Data.Bool (bool)
 import safe Data.List (sort)
 
 main :: IO ()
@@ -10,13 +11,13 @@ main =
     lines >>> drop 1
       >>> map (words >>> map read)
       >>> process
-      >>> map (\b -> if b then "YES" else "NO")
+      >>> map (bool "NO" "YES")
       >>> unlines
 
 process :: [[Int]] -> [Bool]
 process [] = []
-process ([_] : xs : rest) = solve xs : process rest
+process (_ : xs : rest) = solve xs : process rest
 
 solve :: [Int] -> Bool
 solve [_] = True
-solve xs = let xs' = sort xs in maximum (zipWith (-) (drop 1 xs') xs') <= 1
+solve xs = let xs' = sort xs in maximum (zipWith (-) (tail xs') xs') <= 1
