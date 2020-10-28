0dd1ac6ac57a5dc49d6c07263eab019b64b5fe50
anurudhp <anurudhp97@gmail.com>
2020-10-28 09:15:42 +0530

    update kattis/1745

--- contests/cses/1745.hs
+++ contests/cses/1745.hs
@@ -6,7 +6,7 @@ main :: IO ()
 main = interact $ words >>> drop 1 >>> map read >>> process >>> unlines
 
 process :: [Int] -> [String]
-process xs = let ys = solve xs in [show . length, unwords . map show] <*> pure ys
+process xs = [show . length, unwords . map show] <*> [solve xs]
 
 solve :: [Int] -> [Int]
 solve = drop 1 . map fst . filter snd . zip [0 ..] . foldl f [True]
